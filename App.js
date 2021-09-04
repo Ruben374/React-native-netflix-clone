3e2765a15a276f175a7a9be2895f465b6048e367
Ruben André <62626341+Ruben374@users.noreply.github.com>
2021-09-04 23:18:21 +0100

    v

--- App.js
+++ App.js
@@ -1,20 +1,3 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import Preload from './src/pages/preload'
-import { createStackNavigator } from '@react-navigation/stack'
 
-const Stack = createStackNavigator()
-
-export default function App(){
-return(
-
-      <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name='preload' component={Preload} /> 
-    </Stack.Navigator>
-      </NavigationContainer>
-
-  );
-}
 
 
