69cde35f0c937dae835371b49a1b4452f6081025
Ruben André <62626341+Ruben374@users.noreply.github.com>
2021-09-04 22:59:20 +0100

    v

---
+++ src/pages/preload/index.js
@@ -0,0 +1,11 @@
+import React,{ useEffect } from 'react'
+import {View,Text} from 'react-native'
+
+export default()=>{
+
+    return(
+    <View>
+       <Text>ola mundo</Text>
+    </View>
+    )
+}
